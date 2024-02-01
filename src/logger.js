dad7c8ab8e877ae03edac9ae1468fd856f874209
Tan Yi Jun <tanyijun6@gmail.com>
2024-02-01 19:15:43 +0800

    Add log normal message

--- src/logger.js
+++ src/logger.js
@@ -2,6 +2,12 @@
   to log tasks and projects either to browser or to local storage
 */
 
-export function logTask(task) {
+export { logTask, logMessage };
+
+function logTask(task) {
   console.log(task);
+}
+
+function logMessage(message) {
+  console.log(message);
 }
