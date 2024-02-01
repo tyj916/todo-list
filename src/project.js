e635ddd187d6cb0ab57ec74445c6be25e745079a
Tan Yi Jun <tanyijun6@gmail.com>
2024-02-01 18:55:14 +0800

    Create project object

--- src/project.js
+++ src/project.js
@@ -1,8 +1,32 @@
 /*
   projects are going to organize tasks, so it contains:
-  - task
+  - title
+  - description
+  - tasks
   
   functions:
   - update project
 */
 
+import { logTask } from "./logger";
+
+export { addProject };
+
+function addProject(title, description) {
+  const tasks = [];
+
+  const addTask = (task) => {
+    tasks.push(task);
+  }
+
+  const log = () => {
+    tasks.forEach(task => {
+      logTask(task);
+    })
+  }
+
+  return {
+    addTask,
+    log,
+  }
+}
