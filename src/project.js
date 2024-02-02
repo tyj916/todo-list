5cbc6b8fe084028cc6d895d353a9cae883b26774
Tan Yi Jun <tanyijun6@gmail.com>
2024-02-02 12:54:36 +0800

    Add project handler

--- src/project.js
+++ src/project.js
@@ -9,27 +9,48 @@
   - remove task
 */
 
-import { logMessage, logTask } from "./logger";
+import { logMessage, logProject, logTask } from "./logger";
 
-export { createProject };
+export { createProject, projectHandler };
 
 function createProject(title, description, isCompleted) {
   const tasks = [];
-
-  function addTask(task) {
-    tasks.push(task);
-  }
-
-  function removeTask(taskIndex) {
-    tasks.splice(taskIndex, 1);
-  }
 
   return {
     title,
     description,
     isCompleted,
     tasks,
+  }
+}
+
+function projectHandler(project) {
+  function addTask(task) {
+    project.tasks.push(task);
+  }
+
+  function update(title, description) {
+    project.title = title;
+    project.description = description;
+  }
+
+  function complete() {
+    project.isCompleted = true;
+  }
+
+  function removeTask(taskIndex) {
+    project.tasks.splice(taskIndex, 1);
+  }
+
+  function log() {
+    logProject(project);
+  }
+
+  return {
     addTask,
+    update,
+    complete,
     removeTask,
+    log,
   }
 }
